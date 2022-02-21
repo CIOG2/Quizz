e46ad8ba8ac97975847d629dd8c83c5a58860fcb
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-21 16:01:45 -0700

    Archivo que returna una lista con un orden direfente

---
+++ src/JS/utils/ramdonNumbers.js
@@ -0,0 +1,13 @@
+const ramdonNumbers = (list) => {
+
+    let elements = list.length;
+    let newList = [];
+
+    for (let i = 0; i < elements; i++) {
+        const ramdon = Math.floor(Math.random() * list.length);
+        newList.push(list[ramdon]);
+        list.splice(ramdon, 1);
+    }
+    
+    return newList;
+}
