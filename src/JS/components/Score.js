b657a5acf35af4d7d251a4976b1eaa22c6c1b285
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-01 09:55:46 -0700

    Comentarios para el archivo de score.js

--- src/JS/components/Score.js
+++ src/JS/components/Score.js
@@ -1,18 +1,21 @@
 import { localStorage } from "../utils/localStorage.js";
 
+//La funcion Score se encarga de mostrar el puntaje y los record que el usuario tiene
 const Score = () => {
     
+    //Se crean elementos ul donde se van a mostrar los records
     const ListaRecords = document.createElement('ul')
     ListaRecords.classList.add('lista__records')
     
     const title = document.createElement('h2')
     title.classList.add('score__title');
     
+    //Se traen los valores del localStorage
     const storage = localStorage().get("QuizzGame");
-
+    //Si el localStorage tiene datos comienza a renderizar todos los records que tiene en las etiquetas li 
+    //Si no tiene datos este paso se omite
     if (storage) {
         storage.forEach((item) => {
-        
             const nombre = document.createElement('h3');
             nombre.classList.add('lista__records--iteam-name');
             nombre.textContent = item.name;
@@ -29,12 +32,13 @@ const Score = () => {
         });
     }
 
-
+    //Logo de reintentar
     const imgRetry = document.createElement('img');
     imgRetry.classList.add('img__retry');
     imgRetry.src = 'https://i.ibb.co/GdH6xC5/reset.png';
     imgRetry.alt = 'retry';
 
+    //Elemento que reinicia el juego
     const retryButton = document.createElement('button');
     retryButton.classList.add('retry__button');
     retryButton.append(imgRetry);
@@ -42,11 +46,13 @@ const Score = () => {
         location.reload();
     });
 
-
+    //Contenedor principal de los records
     const containerScore = document.createElement('div');
     containerScore.classList.add('container__Score');
     
     
+    //Si el localStorage tiene datos se renderiza la lista de records
+    //Si no tiene datos no la carga
     if (storage) {
         title.textContent = 'SCORES';
         containerScore.append( title, ListaRecords, retryButton );
