adec1757b50ee89a7de30fbdb03d2523a9e2cb48
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-02 17:18:57 -0700

    Cambie el tiempo ademas de aÃadir mas preguntas

--- src/JS/data/preguntas.js
+++ src/JS/data/preguntas.js
@@ -25,9 +25,71 @@ const data = [
     },
     {
         pregunta: '¿En que año llegó el hombre a la luna?',
-        opciones: ['20 de julio de 1958', '20 de julio de 1959', '20 de julio de 1969', '20 de julio de 1959'],
+        opciones: ['20 de julio de 1958', '20 de julio de 1968', '20 de julio de 1969', '20 de julio de 1959'],
         respuesta: '20 de julio de 1969',
         image: 'https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/45XRPL3JRRH5TGBHZCWEL6HC3M.jpg'
+    },
+    {
+        pregunta: '¿Cuál es el lugar más frío de la tierra?',
+        opciones: ['Antártida', 'Polo norte', 'Canada', 'Alaska'],
+        respuesta: 'Antártida',
+        image: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/55D6/production/_115747912_4-1.jpg'
+    },
+    {
+        pregunta: '¿Cuál es el río más largo del mundo?',
+        opciones: ['Rio Bravo', 'Rio Amazonas', 'Río Nilo', 'Río Niger'],
+        respuesta: 'Río Nilo',
+        image: 'https://www.radioenciclopedia.cu/images/images/cuba-2018/Julio/rio-nilo.jpg'
+    },{
+        pregunta: '¿Dónde originaron los juegos olímpicos?',
+        opciones: ['Grecia', 'Rusia', 'Alemania', 'Estados Unidos'],
+        respuesta: 'Grecia',
+        image: 'https://astileku.eus/wp-content/uploads/2018/02/300px-Olympic_flag.svg.png'
+    },{
+        pregunta: '¿Cuál es el océano más grande?',
+        opciones: ['Océano Atlántico', 'Océano Pacífico', 'Océano Indico', 'Océano Atlántico'],
+        respuesta: 'Océano Pacífico',
+        image: 'https://concepto.de/wp-content/uploads/2018/09/oc%C3%A9ano-e1537912452874.jpg'
+    },{
+        pregunta: '¿En qué año llegó Cristóbal Colón a América?',
+        opciones: [ '1492', '1494', '1496', '1498'],
+        respuesta: '1492',
+        image: 'http://4.bp.blogspot.com/-atDvzZ1uS5c/VHtNPWgfC_I/AAAAAAAACpU/7AITErAJb6E/s1600/Las%2Bcarabelas%2Bdel%2Bdescubrimiento.jpg'
+    },{
+        pregunta: '¿Quién es el famoso Rey del Rock en los Estados Unidos?',
+        opciones: ['Elton John', 'Elvis Presley', 'Elvis Costello', 'Michael Jackson'],
+        respuesta: 'Elvis Presley',
+        image: 'http://www.biggigbag.com/archivos/articulos/rockandroll.jpg'
+    },{
+        pregunta: '¿Cómo se llama el nuevo presidente de los Estados Unidos?',
+        opciones: [ 'Donald Trump', 'Barack Obama', 'George Bush', 'Joseph Biden'],
+        respuesta: 'Joseph Biden',
+        image: 'https://www.ambientum.com/wp-content/uploads/2019/11/bandera-estados-unidos.jpg'
+    },{
+        pregunta: '¿Cuál es el país más grande del mundo?',
+        opciones: [ 'China', 'Rusia', 'Japón', 'Canada'],
+        respuesta: 'Rusia',
+        image: 'https://concepto.de/wp-content/uploads/2020/03/globo-terraqueo-e1583703184845.jpg'
+    },{
+        pregunta: 'Si 50 es el 100%, ¿cuánto es el 90%?',
+        opciones: [ '45', '35', '100', '10'],
+        respuesta: '45',
+        image: 'https://clubdeltrade.com/wp-content/uploads/2019/03/Como-mensurar-os-resultados-de-Trade-Marketing.png'
+    },{
+        pregunta: '¿Cuál es tercer planeta en el sistema solar?',
+        opciones: ['Marte', 'Júpiter', 'Tierra', 'Urano'],
+        respuesta: 'Tierra',
+        image: 'https://www.astromia.com/solar/fotos/sistemasolar1.jpg'
+    },{
+        pregunta: '¿Qué país tiene forma de bota?',
+        opciones: ['Francia', 'Italia', 'Inglaterra', 'España'],
+        respuesta: 'Italia',
+        image: 'https://turismo.org/wp-content/uploads/2013/06/italai-en-europa-a.jpg'
+    },{
+        pregunta: '¿Cuál es la moneda del Reino Unido?',
+        opciones: ['Dólar', 'Euro', 'Libra', 'Yen'],
+        respuesta: 'Libra',
+        image: 'https://www.eliberico.com/wp-content/uploads/2021/09/libra-esterlina.jpg'
     },
 ];
 
